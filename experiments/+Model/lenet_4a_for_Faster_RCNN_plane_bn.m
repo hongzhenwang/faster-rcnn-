d993e6981601d275f35977863b2db5fde6028522
hongzhenwang <hongzhen.wang01@gmail.com>
2016-01-14 14:11:53 +0800

    add bn to lenet not work

---
+++ experiments/+Model/lenet_4a_for_Faster_RCNN_plane_bn.m
@@ -0,0 +1,45 @@
+function model = lenet_4a_for_Faster_RCNN_plane_bn(model)
+
+model.mean_image                                = fullfile(pwd, 'models', 'pre_trained_models', 'goolenet', 'mean_image');
+% model.pre_trained_net_file                      = fullfile(pwd, 'output', 'rpn_cachedir', 'faster_rcnn_VOC2007_ZF_stage1_rpn', 'voc_2007_trainval','final');
+model.pre_trained_net_file                      = fullfile(pwd,  'models', 'pre_trained_models', 'goolenet','bvlc_googlenet.caffemodel' );
+% Stride in input image pixels at the last conv layer
+model.feat_stride                               = 16;
+
+%% stage 1 rpn, inited from pre-trained network
+model.stage1_rpn.solver_def_file                = fullfile(pwd, 'models', 'rpn_prototxts', 'lenet_stage1', 'solver_5k10k(bn).prototxt');
+model.stage1_rpn.test_net_def_file              = fullfile(pwd, 'models', 'rpn_prototxts', 'lenet_stage1', 'test_plane_4a(bn).prototxt');
+model.stage1_rpn.init_net_file                  = model.pre_trained_net_file;
+
+% rpn test setting
+model.stage1_rpn.nms.per_nms_topN              	= -1;
+model.stage1_rpn.nms.nms_overlap_thres        	= 0.7;
+model.stage1_rpn.nms.after_nms_topN           	= 2000;
+
+%% stage 1 fast rcnn, inited from pre-trained network
+model.stage1_fast_rcnn.solver_def_file          = fullfile(pwd, 'models', 'fast_rcnn_prototxts', 'lenet_stage1', 'solver_5k10k(bn).prototxt');
+model.stage1_fast_rcnn.test_net_def_file        = fullfile(pwd, 'models', 'fast_rcnn_prototxts', 'lenet_stage1', 'test_plane_4a(bn).prototxt');
+model.stage1_fast_rcnn.init_net_file            = model.pre_trained_net_file;
+
+%% stage 2 rpn, only finetune fc layers
+model.stage2_rpn.solver_def_file                = fullfile(pwd, 'models', 'rpn_prototxts', 'lenet_stage2', 'solver_5k10k(bn).prototxt');
+model.stage2_rpn.test_net_def_file              = fullfile(pwd, 'models', 'rpn_prototxts', 'lenet_stage2', 'test_plane_4a(bn).prototxt');
+
+% rpn test setting
+model.stage2_rpn.nms.per_nms_topN             	= -1;
+model.stage2_rpn.nms.nms_overlap_thres       	= 0.7;
+model.stage2_rpn.nms.after_nms_topN           	= 2000;
+
+%% stage 2 fast rcnn, only finetune fc layers
+model.stage2_fast_rcnn.solver_def_file          = fullfile(pwd, 'models', 'fast_rcnn_prototxts', 'lenet_stage2', 'solver_5k10k(bn).prototxt');
+model.stage2_fast_rcnn.test_net_def_file        = fullfile(pwd, 'models', 'fast_rcnn_prototxts', 'lenet_stage2', 'test_plane_4a(bn).prototxt');
+
+%% stage  ori, only finetune fc layers
+model.stage_ori.solver_def_file          = fullfile(pwd, 'models', 'ori_prototxts', 'lenet_with_bbox', 'solver_5k10k(bn).prototxt');
+model.stage_ori.test_net_def_file        = fullfile(pwd, 'models', 'ori_prototxts', 'lenet_with_bbox', 'test_plane_4a(bn).prototxt');
+
+%% final test
+model.final_test.nms.per_nms_topN            	= 6000; % to speed up nms
+model.final_test.nms.nms_overlap_thres       	= 0.7;
+model.final_test.nms.after_nms_topN          	= 300;
+end
